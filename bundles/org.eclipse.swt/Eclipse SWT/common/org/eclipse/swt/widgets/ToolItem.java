5e899913d7ea9e6483311edd68e7252d8f8cda72
Dieter Mai <maidieter@googlemail.com>
2025-02-15 20:19:12 +0100

    minor change

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/widgets/ToolItem.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/widgets/ToolItem.java
@@ -741,12 +741,6 @@ public class ToolItem extends Item {
 		internalSelect();
 	}
 
-	@Override
-	boolean setTabItemFocus() {
-		NOT_IMPLEMENTED();
-		return false;
-	}
-
 	/**
 	 * Sets the receiver's tool tip text to the argument, which may be null
 	 * indicating that the default tool tip for the control will be shown. For a
